ad0ba6bbefb4d7febbddee1fda2b96944f18a389
Sachin <hellosachindev@gmail.com>
2018-07-07 01:01:45 +0530

    10

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -159,3 +159,20 @@ print("Number of odd numbers :",count_odd)
 
 
 
+
+# Write a Python program to get the Fibonacci series between 0 to 50
+x,y=0,1
+while y<50:
+    print(y)
+    x,y = y,x+y
+
+
+#  Write a Python program to find those numbers which are divisible by 7 and multiple of 5, between 1500 and 2700
+nl=[]
+for x in range(1500, 2700):
+    if (x%7==0) and (x%5==0):
+        nl.append(str(x))
+print (','.join(nl))
+
+
+
