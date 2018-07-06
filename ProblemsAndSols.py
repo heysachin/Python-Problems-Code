d3e6eceb9ac3d683f973d66b68fb0b80f6584e14
Sachin <hellosachindev@gmail.com>
2018-07-07 01:04:59 +0530

    21

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -385,3 +385,18 @@ import itertools
 print(list(itertools.permutations([1,2,3])))
 
 
+
+
+# Write a Python program to remove duplicates from a list.
+# Input a = [10,20,30,20,10,50,60,40,80,50,40]
+# Output [10, 20, 30, 50, 60, 40, 80]
+a = [10,20,30,20,10,50,60,40,80,50,40]
+dup_items = set()
+uniq_items = []
+for x in a:
+    if x not in dup_items:
+        uniq_items.append(x)
+        dup_items.add(x)
+print(uniq_items)
+
+
