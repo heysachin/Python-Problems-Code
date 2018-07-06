1d0a33ccc9f4b182ccb00cd4f775d2b44b5ece25
Sachin <hellosachindev@gmail.com>
2018-07-07 01:01:00 +0530

    6

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -80,3 +80,18 @@ length3 = int(input('enter side 3\n'))
 triangle_check(length1,length2,length3)
 
 
+
+# Write a Python program to construct the following pattern, using a nested loop number.
+# 1                                                                                                             
+# 22                                                                                                            
+# 333                                                                                                           
+# 4444                                                                                                          
+# 55555                                                                                                         
+# 666666                                                                                                        
+# 7777777                                                                                                       
+# 88888888                                                                                                      
+# 999999999
+for i in range(10):
+    print(str(i) * i)
+
+
