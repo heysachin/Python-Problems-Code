7d50db899aa5cfe27161ef838c5643c376dba614
Sachin <hellosachindev@gmail.com>
2018-07-07 01:12:30 +0530

    46

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -834,3 +834,35 @@ class py_solution:
 print(py_solution().reverse_words('hello world'))
 
 
+
+
+# Write a Python program to find the three elements that sum to zero from a set (array) of n real numbers.
+# Input
+# [-25, -10, -7, -3, 2, 4, 8, 10]
+# Output
+# [[-10, 2, 8], [-7, -3, 10]]
+class py_solution:
+ def threeSum(self, nums):
+        nums, result, i = sorted(nums), [], 0
+        while i < len(nums) - 2:
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                if nums[i] + nums[j] + nums[k] < 0:
+                    j += 1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                else:
+                    result.append([nums[i], nums[j], nums[k]])
+                    j, k = j + 1, k - 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+            i += 1
+            while i < len(nums) - 2 and nums[i] == nums[i - 1]:
+                i += 1
+        return result
+print(py_solution().threeSum([-25, -10, -7, -3, 2, 4, 8, 10]))
+ 
+
+
