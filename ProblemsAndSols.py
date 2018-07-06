2a1d5f34e7a5edbec26d638d18ea3f3e6695a424
Sachin <hellosachindev@gmail.com>
2018-07-07 01:01:34 +0530

    9

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -138,3 +138,24 @@ print("Digits", d)
 
 
 
+# Count the number of even and odd numbers from a series of numbers
+# Input 
+# numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) # Declaring the tuple
+# Output
+# Number of even numbers : 4                                                                                    
+# Number of odd numbers : 5
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) # Declaring the tuple
+count_odd = 0
+count_even = 0
+for x in numbers:
+        if not x % 2:
+        count_even+=1
+        else:
+        count_odd+=1
+                
+print("Number of even numbers :",count_even)
+print("Number of odd numbers :",count_odd)
+
+
+
+
