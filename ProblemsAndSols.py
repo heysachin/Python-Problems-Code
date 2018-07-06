f3b801e1abd33e725835ec7e413cdf6eb9c6a6b7
Sachin <hellosachindev@gmail.com>
2018-07-07 01:01:54 +0530

    11

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -176,3 +176,28 @@ print (','.join(nl))
 
 
 
+# Generate a random number between 1 and 9 (including 1 and 9).
+# Ask the user to guess the number, then tell them whether they guessed too low, too high, or exactly right.
+import random
+number = random.randint(1,9)
+guess = 0
+count = 0
+while guess != number and guess != "exit":
+    guess = input("What's your guess?")
+    
+    if guess == "exit":
+        break
+    
+    guess = int(guess)
+    count += 1
+    
+    if guess < number:
+        print("Too low!")
+    elif guess > number:
+        print("Too high!")
+    else:
+        print("You got it!")
+        print("And it only took you",count,"tries!")
+
+
+
