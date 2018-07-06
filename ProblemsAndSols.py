e7ae818f02eae3c8ccc5145dc9446d2ab530f429
Sachin <hellosachindev@gmail.com>
2018-07-07 01:08:10 +0530

    34

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -650,3 +650,10 @@ print('Dictionary in descending order by value : ',sorted_d)
 
 
 
+# Write a Python script to print a dictionary where the keys are numbers between 1 and 15 (both included) and the values are square of keys.
+d=dict()
+for x in range(1,16):
+    d[x]=x**2
+print(d)
+
+
