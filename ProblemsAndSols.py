2a8d3a2328c87037ceccc1c007c91cffe602bfde
Sachin <hellosachindev@gmail.com>
2018-07-07 01:00:47 +0530

    5

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -65,3 +65,18 @@ for row in range(row_num):
 print(multi_list)
 
 
+
+# Write a Python program to check a triangle is valid or not
+def triangle_check(l1,l2,l3):
+    if (l1>l2+l3) or (l2>l1+l3) or (l3>l1+l2):
+        print('No, the lengths wont form a triangle')
+    elif (l1==l2+l3) or (l2==l1+l3) or (l3==l1+l2):
+        print('yes, it can form a degenerated triangle')
+    else:
+        print('Yes, a triangle can be formed out of it')
+length1 = int(input('enter side 1\n'))
+length2 = int(input('enter side 2\n'))
+length3 = int(input('enter side 3\n'))
+triangle_check(length1,length2,length3)
+
+
