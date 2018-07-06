3e295b3744de7716425bb3c792198212cbcc9c9c
Sachin <hellosachindev@gmail.com>
2018-07-07 01:03:52 +0530

    18

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -348,3 +348,18 @@ list2 = [1, 2]
 print(list(set(list1) - set(list2)))
 
 
+
+
+# Write a Python program to get the smallest number from a list.
+# max_num_in_list([1, 2, -8, 0])
+# return 2
+def max_num_in_list( list ):
+    max = list[ 0 ]
+    for a in list:
+        if a > max:
+            max = a
+    return max
+print(max_num_in_list([1, 2, -8, 0]))
+
+
+
