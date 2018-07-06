02b824b8c013ab13449f6112e4d591f0592fec7d
Sachin <hellosachindev@gmail.com>
2018-07-07 01:07:35 +0530

    31

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -590,3 +590,23 @@ print( word_count('the quick brown fox jumps over the lazy dog.'))
 
 #              Dictionary
 
+
+
+# Check if a given key already exists in a dictionary
+# input
+# d = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+# is_key_present(5)
+# is_key_present(9)
+# output
+# Key is present in the dictionary                                                                              
+# Key is not present in the dictionary
+d = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+def is_key_present(x):
+  if x in d:
+      print('Key is present in the dictionary')
+  else:
+      print('Key is not present in the dictionary')
+is_key_present(5)
+is_key_present(9)
+
+
