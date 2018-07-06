91a18273a1a0b5d936b3a3f442ad8608c6d01045
Sachin <hellosachindev@gmail.com>
2018-07-07 01:12:09 +0530

    44

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -810,3 +810,16 @@ str1.get_String()
 str1.print_String()
 
 
+
+
+# Write a Python class named Rectangle constructed by a length and width and a method which will compute the area of a rectangle.
+class Rectangle():
+    def __init__(self, l, w):
+        self.length = l
+        self.width  = w
+    def rectangle_area(self):
+        return self.length*self.width
+newRectangle = Rectangle(12, 10)
+print(newRectangle.rectangle_area())
+
+
