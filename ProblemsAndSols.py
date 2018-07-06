db674d7bc01f6a513144f805ebe289b8994e1ab7
Sachin <hellosachindev@gmail.com>
2018-07-07 01:10:34 +0530

    37

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -680,3 +680,15 @@ def file_read_from_head(fname, nlines):
                         
 file_read_from_head('test.txt',2)
 
+
+
+# Write a python program to find the longest words in a file
+# Using text.txt file in same folder
+def longest_word(filename):
+    with open(filename, 'r') as infile:
+              words = infile.read().split()
+    max_len = len(max(words, key=len))
+    return [word for word in words if len(word) == max_len]
+print(longest_word('test.txt'))
+
+
