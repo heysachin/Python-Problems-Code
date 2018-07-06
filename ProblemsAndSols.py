6d76acd8905e4145c30a907b2062e99cb937f737
Sachin <hellosachindev@gmail.com>
2018-07-07 01:07:55 +0530

    33

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -628,3 +628,25 @@ for d in (dic1, dic2, dic3): dic4.update(d)
 print(dic4)
 
 
+
+
+# Write a Python program to iterate over dictionaries using for loops.
+d = {'x': 10, 'y': 20, 'z': 30} 
+for dict_key, dict_value in d.items():
+    print(dict_key,'->',dict_value)
+
+
+# Write a Python program to sort (ascending and descending) a dictionary by value.
+# Original dictionary :  {0: 0, 1: 2, 2: 1, 3: 4, 4: 3}                                                         
+# Dictionary in ascending order by value :  [(0, 0), (1, 2), (2, 1), (3, 4), (4, 3)]                            
+# Dictionary in descending order by value :  [(4, 3), (3, 4), (2, 1), (1, 2), (0, 0)]
+import operator
+d = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+print('Original dictionary : ',d)
+sorted_d = sorted(d.items(), key=operator.itemgetter(0))
+print('Dictionary in ascending order by value : ',sorted_d)
+sorted_d = sorted(d.items(), key=operator.itemgetter(0),reverse=True)
+print('Dictionary in descending order by value : ',sorted_d)
+
+
+
