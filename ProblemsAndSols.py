4c3a6cf90efb1bbae08cec8c4c08acd4ea3694eb
Sachin <hellosachindev@gmail.com>
2018-07-07 01:13:21 +0530

    49

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -903,3 +903,14 @@ print('Heads:', results['heads'])
 print('Tails:', results['tails'])
 
 
+
+
+#  Write a Python program to generate a series of unique random numbers
+import random
+choices = list(range(100))
+random.shuffle(choices)
+print(choices.pop())
+while choices:
+    print(choices.pop())
+
+
