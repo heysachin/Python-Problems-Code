c80a710ca774ab90cd280b9042b888eaad4fcfd8
Sachin <hellosachindev@gmail.com>
2018-07-07 01:11:47 +0530

    42

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -765,3 +765,31 @@ class py_solution:
 print(py_solution().roman_to_int('MMMCMLXXXVI'))
 print(py_solution().roman_to_int('MMMM'))
 print(py_solution().roman_to_int('C'))
+
+
+
+# Write a Python program to convert an integer to a roman numeral.
+class py_solution:
+    def int_to_Roman(self, num):
+        val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+            ]
+        syb = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+            ]
+        roman_num = ''
+        i = 0
+        while  num > 0:
+            for _ in range(num // val[i]):
+                roman_num += syb[i]
+                num -= val[i]
+            i += 1
+        return roman_num
+print(py_solution().int_to_Roman(1))
+print(py_solution().int_to_Roman(4000))
