030cc270f5449c5c1ef82ced170f7e868fe27c28
Sachin <hellosachindev@gmail.com>
2018-07-07 01:06:34 +0530

    27

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -527,3 +527,16 @@ print(find_longest_word(["PHP", "Exercises", "Backend"]))
 
 
 
+
+
+# Write a Python program to remove the nth index character from a nonempty string
+def remove_char(str, n):
+      first_part = str[:n] 
+      last_pasrt = str[n+1:]
+      return first_part + last_pasrt
+    
+print(remove_char('Python', 0))
+print(remove_char('Python', 3))
+print(remove_char('Python', 5))
+
+
