dbdc21e2cd3987be4c6d1251cf78541b15dd7279
Sachin <hellosachindev@gmail.com>
2018-07-07 01:11:33 +0530

    41

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -741,3 +741,27 @@ class Circle():
 NewCircle = Circle(8)
 print(NewCircle.area())
 print(NewCircle.perimeter())
+
+
+# Write a Python class to convert a roman numeral to an integer
+# Sample input
+# 'MMMCMLXXXVI'
+# 'MMMM'
+# 'C'
+# Sample output
+# 3986                                                                                                          
+# 4000                                                                                                          
+# 100
+class py_solution:
+    def roman_to_int(self, s):
+        rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        int_val = 0
+        for i in range(len(s)):
+            if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
+                int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
+            else:
+                int_val += rom_val[s[i]]
+        return int_val
+print(py_solution().roman_to_int('MMMCMLXXXVI'))
+print(py_solution().roman_to_int('MMMM'))
+print(py_solution().roman_to_int('C'))
