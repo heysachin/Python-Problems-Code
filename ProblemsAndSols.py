26fee6378638608722400b77063701c73e1d1607
Sachin <hellosachindev@gmail.com>
2018-07-07 01:04:49 +0530

    20

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -376,3 +376,12 @@ ctr = collections.Counter(my_list)
 print("Frequency of the elements in the List : ",ctr)
 
 
+
+
+#  Write a Python program to generate all permutations of a list in Python
+# Input [1,2,3]
+# Output [(1, 2, 3), (1, 3, 2), (2, 1, 3), (2, 3, 1), (3, 1, 2), (3, 2, 1)]
+import itertools
+print(list(itertools.permutations([1,2,3])))
+
+
