f649532f04f7e415a7dfde3c7f53fd5b00895011
Sachin <hellosachindev@gmail.com>
2018-07-07 01:03:26 +0530

    16

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -324,3 +324,15 @@ print(is_Sublist(a, b))
 print(is_Sublist(a, c))
 
 
+
+# Write a Python program to find common items from two lists.
+# input
+# color1 = "Red", "Green", "Orange", "White"
+# color2 = "Black", "Green", "White", "Pink"
+# output
+# {'Green', 'White'}
+color1 = "Red", "Green", "Orange", "White"
+color2 = "Black", "Green", "White", "Pink"
+print(set(color1) & set(color2))
+
+
