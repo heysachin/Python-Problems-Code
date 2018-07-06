a879abf3e597d5e1d41d6459e5e305f734b1964d
Sachin <hellosachindev@gmail.com>
2018-07-07 00:58:54 +0530

    Prob 1

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -1,3 +1,22 @@
 #<-------- Part 1 Fundamentals --------->
 
 #              Basics
+
+
+
+# Write a Python program to print the following string in a specific format (see the output). Go to the editor
+# Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are" 
+# Output :
+# Twinkle, twinkle, little star,
+#     How I wonder what you are! 
+#         Up above the world so high,
+#         Like a diamond in the sky. 
+# Twinkle, twinkle, little star, 
+#     How I wonder what you are
+# ----------------------------------------------------
+# Hints
+# Using \n (newline) and \t (tab) to format the string
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!") 
+
+
+
