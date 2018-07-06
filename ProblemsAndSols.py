ad498f3b32ad5b48dd1d99acc4859ed5be097f83
Sachin <hellosachindev@gmail.com>
2018-07-07 01:10:42 +0530

    38

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -692,3 +692,13 @@ def longest_word(filename):
 print(longest_word('test.txt'))
 
 
+
+
+# Write a Python program to read a random line from a file.
+# Using test.txt
+import random
+def random_line(fname):
+    lines = open(fname).read().splitlines()
+    return random.choice(lines)
+print(random_line('test.txt'))
+
