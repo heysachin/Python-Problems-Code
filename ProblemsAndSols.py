a23df70f2e55790ce8bdcede58d794ef3d1fa70b
Sachin <hellosachindev@gmail.com>
2018-07-07 01:11:19 +0530

    40

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -726,3 +726,18 @@ print(py_solution().is_valid_parenthese("()[{)}"))
 print(py_solution().is_valid_parenthese("()"))
 
 
+
+
+# Write a Python class named Circle constructed by a radius and two methods which
+# will compute the area and the perimeter of a circle.
+class Circle():
+    def __init__(self, r):
+        self.radius = r
+    def area(self):
+        return self.radius**2*3.14
+    
+    def perimeter(self):
+        return 2*self.radius*3.14
+NewCircle = Circle(8)
+print(NewCircle.area())
+print(NewCircle.perimeter())
