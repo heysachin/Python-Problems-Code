56b9335804adf51d0490db0601ee460537a1cc38
Sachin <hellosachindev@gmail.com>
2018-07-07 01:04:03 +0530

    19

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -363,3 +363,16 @@ print(max_num_in_list([1, 2, -8, 0]))
 
 
 
+
+# Write a Python program to get the frequency of the elements in a list.
+# input
+# my_list = [10,10,10,10,20,20,20,20,40,40,50,50,30]
+# outout
+# {10: 4, 20: 4, 40: 2, 50: 2, 30: 1}
+import collections
+my_list = [10,10,10,10,20,20,20,20,40,40,50,50,30]
+print("Original List : ",my_list)
+ctr = collections.Counter(my_list)
+print("Frequency of the elements in the List : ",ctr)
+
+
