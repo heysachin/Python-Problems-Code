3600f8b937f91fea3471a60e01474520cb4e6761
Sachin <hellosachindev@gmail.com>
2018-07-07 01:01:23 +0530

    8

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -118,3 +118,23 @@ for i in range(n,0,-1):
 
 
 
+# Write a Python program that accepts a string and calculate the number of digits and letters
+# Sample Data : "Python 3.2"
+# Expected Output :
+# Letters 6 
+# Digits 2
+s = input("Input a string")
+d=l=0
+for c in s:
+    if c.isdigit():
+        d=d+1
+    elif c.isalpha():
+        l=l+1
+    else:
+        pass
+print("Letters", l)
+print("Digits", d)
+
+
+
+
