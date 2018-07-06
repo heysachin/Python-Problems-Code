61bd55d548aa14ce51abe2c140ac34f24194708c
Sachin <hellosachindev@gmail.com>
2018-07-07 01:00:19 +0530

    Condition & Loop

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -37,3 +37,10 @@ name = input("What is your name: ")
 age = int(input("How old are you: "))
 year = str((2014 - age)+100)
 print(name + " will be 100 years old in the year " + year)
+
+
+
+
+#              Condition & Loop
+
+
