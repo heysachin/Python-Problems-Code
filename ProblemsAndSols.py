c9150d07ad3df3b609f30121f1063bdee287883c
Sachin <hellosachindev@gmail.com>
2018-07-07 01:12:18 +0530

    45

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -823,3 +823,14 @@ newRectangle = Rectangle(12, 10)
 print(newRectangle.rectangle_area())
 
 
+
+
+# Write a Python class to reverse a string word by word.
+# Input "hello world"
+# Output "world hello"
+class py_solution:
+    def reverse_words(self, s):
+        return ' '.join(reversed(s.split()))
+print(py_solution().reverse_words('hello world'))
+
+
