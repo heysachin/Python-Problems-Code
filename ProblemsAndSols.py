9b90a4175ab4eb8d0ee1a501c07272ba712903d4
Sachin <hellosachindev@gmail.com>
2018-07-07 01:05:10 +0530

    22

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -400,3 +400,21 @@ for x in a:
 print(uniq_items)
 
 
+
+
+# Write a Python program to find the second smallest number in a list.
+# input
+# second_smallest([1, 2, -8, -2, 0])
+# output
+# -2
+def second_smallest(numbers):
+    a1, a2 = float('inf'), float('inf')
+    for x in numbers:
+        if x <= a1:
+            a1, a2 = x, a1
+        elif x < a2:
+            a2 = x
+    return a2
+print(second_smallest([1, 2, -8, -2, 0]))
+
+
