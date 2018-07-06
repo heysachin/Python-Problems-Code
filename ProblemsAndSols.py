b6360a15bef9452dbab00ee81ef3faca5a79f38e
Sachin <hellosachindev@gmail.com>
2018-07-07 01:06:56 +0530

    29

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -554,3 +554,13 @@ print(reverse_string_words("Python Exercises."))
 
 
 
+
+# Write a Python program to calculate the length of a string.
+def string_length(str1):
+    count = 0
+    for char in str1:
+        count += 1
+    return count
+print(string_length('hello world'))
+
+
