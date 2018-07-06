c65b244b6fead30f41f92bc0c2fdb403b7053787
Sachin <hellosachindev@gmail.com>
2018-07-07 01:13:02 +0530

    47

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -873,4 +873,16 @@ print(py_solution().threeSum([-25, -10, -7, -3, 2, 4, 8, 10]))
 
 
 
+# Write a Python program to convert a binary number to decimal number.
+ 
+ 
+b_num = list(input("Input a binary number: "))
+value = 0
+ 
+for i in range(len(b_num)):
+    digit = b_num.pop()
+    if digit == '1':
+        value = value + pow(2, i)
+print("The decimal value of the number is", value)
+
 
