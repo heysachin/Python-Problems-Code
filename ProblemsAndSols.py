3ed1cc769fc3d755f9235161249226aed715b880
Sachin <hellosachindev@gmail.com>
2018-07-07 01:00:36 +0530

    4

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -44,3 +44,24 @@ print(name + " will be 100 years old in the year " + year)
 #              Condition & Loop
 
 
+
+# Write a Python program which takes two digits m (row) and n (column) as input and generates a two-dimensional array. 
+# The element value in the i-th row and j-th column of the array should be i*j.
+# Note :
+# i = 0,1.., m-1 
+# j = 0,1, n-1.
+# Input
+# Input number of rows: 3                                                                                       
+# Input number of columns: 4  
+# Output
+# [[0, 0, 0, 0], [0, 1, 2, 3], [0, 2, 4, 6]]
+ 
+row_num = int(input("Input number of rows: "))
+col_num = int(input("Input number of columns: "))
+multi_list = [[0 for col in range(col_num)] for row in range(row_num)]
+for row in range(row_num):
+    for col in range(col_num):
+        multi_list[row][col]= row*col
+print(multi_list)
+
+
