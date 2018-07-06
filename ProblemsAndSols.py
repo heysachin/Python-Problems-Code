29d30a987bf99fe4202a628830298c6f37cd9f22
Sachin <hellosachindev@gmail.com>
2018-07-07 01:02:17 +0530

    13

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -238,4 +238,34 @@ if x:
  
 
 
- 
+
+
+# Write a Python program to check a triangle is equilateral, isosceles or scalene.
+# Note :
+# An equilateral triangle is a triangle in which all three sides are equal.
+# A scalene triangle is a triangle that has three unequal sides.
+# An isosceles triangle is a triangle with (at least) two equal sides.
+print("Input lengths of the triangle sides: ")
+x = int(input("x: "))
+y = int(input("y: "))
+z = int(input("z: "))
+if x == y == z:
+ print("Equilateral triangle")
+elif x != y != z:
+ print("Scalene triangle")
+else:
+ print("isosceles triangle") 
+    
+
+# Write a Python program to check whether an alphabet is a vowel or consonant
+l = input("Input a letter of the alphabet: ")
+if l in ('a', 'e', 'i', 'o', 'u'):
+    print("%s is a vowel." % l)
+elif l == 'y':
+    print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
+else:
+    print("%s is a consonant." % l) 
+
+
+
+
