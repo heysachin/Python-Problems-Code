b9aab0afaff3d5aac4bf5a0989783133fde3a7fb
Sachin <hellosachindev@gmail.com>
2018-07-07 01:07:45 +0530

    32

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -610,3 +610,21 @@ is_key_present(5)
 is_key_present(9)
 
 
+
+
+# Write a Python script to concatenate following dictionaries to create a new one
+# Input
+# dic1={1:10, 2:20}
+# dic2={3:30, 4:40}
+# dic3={5:50,6:60}
+# Output
+# {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+ 
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+dic4 = {}
+for d in (dic1, dic2, dic3): dic4.update(d)
+print(dic4)
+
+
