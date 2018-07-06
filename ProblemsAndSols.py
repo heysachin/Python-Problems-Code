419deb99bf781329b001c14d27d3195aa54b7d60
Sachin <hellosachindev@gmail.com>
2018-07-07 01:13:33 +0530

    50

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -914,3 +914,17 @@ while choices:
     print(choices.pop())
 
 
+
+    
+# Write a Python function to round up a number to specified digits.
+import math
+def roundup(a, digits=0):
+    n = 10**-digits
+    return round(math.ceil(a / n) * n, digits)
+x = 123.01247
+print("Original  Number: ",x)
+print(roundup(x, 0))
+print(roundup(x, 1))
+print(roundup(x, 2))
+print(roundup(x, 3))
+
