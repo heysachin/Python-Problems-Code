19f059ce822bbabd9a25366d9767f88a951eaf25
Sachin <hellosachindev@gmail.com>
2018-07-19 05:24:47 +0530

    56

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -1008,6 +1008,18 @@ print(milli_sec)
 
 
 
+#  Write a Python program to subtract five days from current date
+from datetime import date, timedelta
+dt = date.today() - timedelta(5)
+print('Current Date :',date.today())
+print('5 days before Current Date :',dt)
+
+
+
+
+
+
+
 
 
 
