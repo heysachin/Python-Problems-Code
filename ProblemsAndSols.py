81ccc4bc8fb45f525bc43d32b6272f49bf153c7e
Sachin <hellosachindev@gmail.com>
2018-07-19 05:29:39 +0530

    62

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -1096,6 +1096,35 @@ for item in items:
 
 
 
+# Remove all whitespaces from a string
+# 
+# Input
+# ' Python    Exercises '
+# Output
+# PythonExercises
+import re
+text1 = ' Python    Exercises '
+print("Original string:",text1)
+print("Without extra spaces:",re.sub(r'\s+', '',text1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
