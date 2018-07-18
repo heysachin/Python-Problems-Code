7dc7e19471fecd5d1f4e4541e1cedd6d3418b895
Sachin <hellosachindev@gmail.com>
2018-07-19 05:26:08 +0530

    57

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -1022,6 +1022,15 @@ print('5 days before Current Date :',dt)
 
 
 
+# Write a Python program to find all five characters long word in a string.
+# Input
+# 'The quick brown fox jumps over the lazy dog.'
+# Output
+# ['quick', 'brown', 'jumps']
+import re
+text = 'The quick brown fox jumps over the lazy dog.'
+print(re.findall(r"\b\w{5}\b", text))
+
 
 
 
