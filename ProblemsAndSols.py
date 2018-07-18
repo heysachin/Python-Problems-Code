e517deefb4d6f55c3e9d81b48729be509a6d5eac
Sachin <hellosachindev@gmail.com>
2018-07-19 05:24:26 +0530

    55

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -1001,7 +1001,10 @@ print("Day of week: ", datetime.date.today().strftime("%A"))
 
 
 
-
+# Write a Python program to get current time in milliseconds in Python
+import time
+milli_sec = int(round(time.time() * 1000))
+print(milli_sec)
 
 
 
