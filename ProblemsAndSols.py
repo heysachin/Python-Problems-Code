2113173efb7d60cc3a385efe318f07da4aa0616a
Sachin <hellosachindev@gmail.com>
2018-07-19 05:23:00 +0530

    52

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -962,3 +962,34 @@ data = [4, 2, 5, 8, 6]
 print("Sample Data: ",data)
 print("Standard Deviation : ",sd_calc(data))
  
+
+#              Datetime
+
+
+#  Write a Python program to convert Year/Month/Day to Day of Year in Python
+import datetime
+today = datetime.datetime.now()
+day_of_year = (today - datetime.datetime(today.year, 1, 1)).days + 1
+print(day_of_year)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
