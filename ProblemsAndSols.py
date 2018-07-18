1b1714f5635af6110977627d6fb3b3f0371ba229
Sachin <hellosachindev@gmail.com>
2018-07-19 05:27:05 +0530

    59

--- ProblemsAndSols.py
+++ ProblemsAndSols.py
@@ -1049,7 +1049,23 @@ print(is_allowed_specific_char("*&%@#!}{"))
 
 
 
-
+# Write a Python program to find the occurrence and position of the substrings within a string.
+# 
+# Input
+# text = 'Python exercises, PHP exercises, C# exercises'
+# pattern = 'exercises'
+# 
+# Output
+# Found "exercises" at 7:16                                                                                     
+# Found "exercises" at 22:31                                                                                    
+# Found "exercises" at 36:45
+import re
+text = 'Python exercises, PHP exercises, C# exercises'
+pattern = 'exercises'
+for match in re.finditer(pattern, text):
+    s = match.start()
+    e = match.end()
+    print('Found "%s" at %d:%d' % (text[s:e], s, e))
 
 
 
